0b4a2957402edfc5a6daa5565628445c6d376a95
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2025-01-27 02:44:02 +0700

    Create rpc.js

---
+++ src/core/network/rpc.js
@@ -0,0 +1,6 @@
+export class RPC {
+  static CHAINID = 57073;
+  static RPCURL = "https://rpc-gel.inkonchain.com";
+  static EXPLORER = "https://explorer.inkonchain.com/";
+  static SYMBOL = "ETH (INKOCHAIN)";
+}
